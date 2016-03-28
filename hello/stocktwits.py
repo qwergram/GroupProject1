47403e9df1cff24805daaa6321218845f4f0084e
Norton Pengra <npengra317@gmail.com>
2016-03-28 14:48:03 -0700

    JSON trimmer function created

--- hello/stocktwits.py
+++ hello/stocktwits.py
@@ -11,5 +11,28 @@ def get_stock_comments(ticker):
     return json.get('messages')
 
 
+def format_into_table(message, focus):
+    to_return = {
+        "social_id": message['id'],
+        "source": "stocktwits",
+        "focus": focus,
+        "popularity": message['reshares']['reshared_count'],
+        "author": message['user']['username'],
+        "author_image": message['user']['avatar_url_ssl'],
+        "created_time": message['created_at'],
+        "content": message['body'],
+        "symbols": [stock['symbol'] for stock in message['symbols']],
+        "urls": message.get('links'),
+    }
+
+    return to_return
+
+
+
 if __name__ == "__main__":
-    get_stock_comments("MSFT")
+    ticker = "MSFT"
+    messages = get_stock_comments(ticker)
+    for index, message in enumerate(messages):
+        message = format_into_table(message, ticker)
+        messages[index] = message
+    import pdb; pdb.set_trace()
