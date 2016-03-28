031c84cfec94ce43a62412e10e91cd666cad0687
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-28 11:50:04 -0700

    Built a model for the yfinance branch Added name to Humans.txt Added route to urls.py for detail.

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length=5, db_index=True, db_tablespace="indexes")
+    name = models.TextField(max_length=100)
+    date = models.DateField(db_index=True, db_tablespace="indexes")
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.IntegerField()
+    adj_close = models.FloatField()
+
+    class Meta:
+        db_tablespace = "tables"
+
+    def __str__(self):              # __unicode__ on Python 2
+        return (
+            self.ticker,
+            self.name,
+            self.date,
+            self.open,
+            self.high,
+            self.low,
+            self.close,
+            self.volume,
+            self.adj_close,
+        )
