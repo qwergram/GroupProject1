75cef8e8a1383057a9fcf168ee0f0d5e2ce0b26e
Rick Tesmond <tesmonrd@gmail.com>
2016-03-28 15:21:13 -0700

    fixed key and secret call

--- hello/twitter/twitter_api.py
+++ hello/twitter/twitter_api.py
@@ -1,8 +1,8 @@
 from client import Client
 import os
 
-key = os.environ.get('Qy7ThBysBdMa1zwosebGYoXXD')
-secret = os.environ.get('bggZOTTkq4gwoyD2K7VuTqtvAqwdbEgXAFnZgHCvVhAQLEsTqj')
+key = os.environ.get('CONSUMER_KEY')
+secret = os.environ.get('CONSUMER_SECRET')
 
 client = Client(key, secret)
 apple = client.request('https://api.twitter.com/1.1/search/tweets.json\
