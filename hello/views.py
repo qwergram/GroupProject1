a70cd3f50e5eee81a207776dbf52d72bc2b8b635
Norton Pengra <npengra317@gmail.com>
2016-03-29 10:20:44 -0700

    index_view returns latest chatter about msft in ticker

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Message, Company
+import random
 
 from .stocktwits import get_stock_comments, format_into_table, save_message
 from .reddit import (
@@ -11,7 +13,9 @@ from .reddit import (
 
 
 def index(request):
-    return render(request, 'index.html')
+    messages = list(Message.objects.filter(focus="MSFT"))
+    random.shuffle(messages)
+    return render(request, 'index.html', {"streamer": messages})
 
 
 def test(request, ticker):
