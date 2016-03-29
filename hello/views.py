45c4d7b17f7451fe2ad6f3959e56194460053762
Norton Pengra <npengra317@gmail.com>
2016-03-28 22:15:51 -0700

    changed ticker to always be handled as uppercase

--- hello/views.py
+++ hello/views.py
@@ -15,12 +15,21 @@ def index(request):
 
 
 def test(request, ticker):
+    ticker = ticker.upper()
     messages = get_stock_comments(ticker)
     for index, message in enumerate(messages):
         message = format_into_table(message, ticker)
         messages[index] = message
         save_message(message)
-    reddit_messages = save_reddit_articles(scrape_reddit(ticker_to_name(get_companies(), ticker)))
+    try:
+        import pdb; pdb.set_trace()
+
+        companies = get_companies()
+        query = ticker_to_name(companies, ticker)
+        reddit_messages = scrape_reddit(ticker, query)
+        save_reddit_articles(reddit_messages)
+    except KeyError:
+        reddit_messages = []
     return JsonResponse(messages + reddit_messages, safe=False)
 
 
