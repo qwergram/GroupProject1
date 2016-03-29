37c3623a28086a331f3098db8700b9713c64e91e
Norton Pengra <npengra317@gmail.com>
2016-03-28 20:54:12 -0700

    Created function for getting most recent articles from reddit

--- hello/company_search.py
+++ hello/company_search.py
@@ -1,6 +1,8 @@
 import json
 import io
+import requests
 
+REDDIT_API_ENDPOINT = "https://api.reddit.com/search?q={}&type=link"
 
 def get_companies():
     with io.open("stocktalk/static/raw_data/symbols_usa_only.json") as f:
@@ -21,3 +23,19 @@ def ticker_to_name(data, ticker):
     if company_data:
         return company_data["Name"]
     raise ValueError("Ticker not found")
+
+
+def scrape_reddit(query):
+    query = query.replace(' ', '+')
+    response = requests.get(REDDIT_API_ENDPOINT.format(query),
+                            headers={"User-Agent": "StockTalk @ https://github.com/qwergram/GroupProject1"})
+    if not response.ok:
+        raise ConnectionError("Reddit didn't like the query, try again later")
+    json_blob = response.json()['data']['children']
+    links = []
+    for post in json_blob:
+        link = post['data']['url']
+        date = post['data']['created_utc']
+        if "reddit.com" not in link:
+            links.append(link)
+    return links
