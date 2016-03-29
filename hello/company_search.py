3494e78b62a71bd765240533d36f88d610486b41
Norton Pengra <npengra317@gmail.com>
2016-03-28 18:53:58 -0700

    Created ticker to company name function

--- hello/company_search.py
+++ hello/company_search.py
@@ -1,6 +1,7 @@
 import json
 import io
 
+
 def get_companies():
     with io.open("stocktalk/static/raw_data/symbols_usa_only.json") as f:
         j = f.read()
@@ -9,3 +10,14 @@ def get_companies():
     for company in list_of_companies:
         dict_of_companies[company['Ticker']] = company
     return dict_of_companies
+
+
+def ticker_to_name(data, ticker):
+    if not isinstance(data, dict):
+        raise ValueError("Invalid company data")
+    if not isinstance(ticker, str):
+        raise ValueError("Invalid ticker")
+    company_data = data.get(ticker)
+    if company_data:
+        return company_data["Name"]
+    raise ValueError("Ticker not found")
