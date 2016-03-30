6bdc7cbdc3159ee9f3ff0c859e9e81bfb56294a0
Norton Pengra <npengra317@gmail.com>
2016-03-30 12:49:11 -0700

    Changed index view to home view for ajax loading

--- stocktalk/urls.py
+++ stocktalk/urls.py
@@ -10,7 +10,8 @@ admin.autodiscover()
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', hello.views.ajax_load, name="load")
+    url(r'^home/$', hello.views.index, name='index'),
     url(r'^detail/$', hello.views.detail, name='detail'),
     url(r'^detail/(?P<ticker>[a-zA-Z]+)/$', hello.views.detail, name='detail'),
     url(r'^check/(?P<ticker>[a-zA-Z]+)/$', hello.views.test),
