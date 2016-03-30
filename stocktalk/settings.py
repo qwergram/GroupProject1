328522b03ab21fd0562143e25d863461ea306273
Norton Pengra <npengra317@gmail.com>
2016-03-30 09:29:28 -0700

    Updated nose_test args

--- stocktalk/settings.py
+++ stocktalk/settings.py
@@ -136,3 +136,12 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=foo,bar',
+]
