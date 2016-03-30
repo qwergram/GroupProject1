f85006a392a8eb2809ab0dc04b6b2810080557d9
Rick Tesmond <tesmonrd@gmail.com>
2016-03-30 11:31:18 -0700

    100 percent coverage with four tests, all passing

--- hello/tests/test_twitter.py
+++ hello/tests/test_twitter.py
@@ -2,33 +2,33 @@ from django.test import TestCase
 from hello.twitter_api import TwitterCli, get_twitter_comments, json_into_table, save_tweets
 from hello.models import Message
 import datetime
-try:
-    from urllib.request import urlopen, Request
-    from urllib.error import HTTPError
-except ImportError:
-    from urllib2 import urlopen, Request, HTTPError
+import io
+import json
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
-true = True
-false = False
-null = None
-
+with io.open("hello/tests/example_twit.json") as samplejson:
+    SAMPLE_JSON = json.loads(samplejson.read())
 
 EXPECTED_TWITTER = {
-    "popularity": 40,
-    "author_image": "http://pbs.twimg.com/profile_images/546024114272468993/W9gT7hZo_normal.png",
-    "source": "twitter",
+    "symbols": [],
+    "author": "Duane Newman",
     "hashtags": [
         "MSFT",
-        "Azure",
-        "Iaas",
+        "Build2016",
+        "mountaindew",
+        "bldwin",
+        "BeverageFail",
+        "HadToDrinkPepsi"
     ],
-    "urls": [],
+    "source": "twitter",
     "focus": "MSFT",
-    "author": "Azure",
-    "symbols": [],
-    "social_id": 17000457,
-    "created_time": "2016-03-29T17:56:35Z",
-    "content": "#MSFT #Azure is the top choice for companies adopting cloud #IaaS. Read via @siliconangle:https://t.co/unbJ9KkFT2 https://t.co/kDMnH8x6Pc"
+    "author_image": "http://pbs.twimg.com/profile_images/558825363619344386/gUN09sSf_normal.jpeg",
+    "urls": [],
+    "created_time": "2009-01-29T21:01:24Z",
+    "popularity": 0,
+    "content": "Hey #MSFT, I thought #Build2016 was a developer conference. Where's the #mountaindew? #bldwin #BeverageFail #HadToDrinkPepsi",
+    "social_id": 19734130
 }
 
 
@@ -47,4 +47,13 @@ class TwitterCase(TestCase):
             resp = client.request("https://api.twitter.com/1.1/search/tweets.json?q=%23MSFT")
             return resp
 
-    def 
+    def test_json_to_table(self):
+        """Test the corrected format from json to db."""
+        jsonified = json_into_table(SAMPLE_JSON, "MSFT")
+        self.assertEqual(EXPECTED_TWITTER, jsonified)
+
+    def test_invalid_json(self):
+        """Test passing in message with invalid ticker."""
+        with self.assertRaises(ValueError):
+            so_true = False
+            json_into_table(SAMPLE_JSON, so_true)
