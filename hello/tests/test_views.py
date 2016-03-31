3c756747a280dabce7b2963da7a647951f9af7f8
Norton Pengra <npengra317@gmail.com>
2016-03-31 10:49:50 -0700

    Wrote test for expecting 404 error

--- hello/tests/test_views.py
+++ hello/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
 # Must call test hello.views.test because there is something in django that
 # uses the same name.
 import hello.views
@@ -8,7 +8,7 @@ from hello.views import (
 )
 
 
-class TestViews(TestCase):
+class TestViews(TransactionTestCase):
     # XXX Because we're modifying the code as we speak, I left four basic tests
     # XXX to increase coverage %.
 
@@ -36,3 +36,7 @@ class TestViews(TestCase):
     def test_reddit_response_code(self):
         response = hello.views.test(None, "reddit", "MSFT")
         self.assertTrue(response.status_code == 200)
+
+    def test_nonexistent_response_code(self):
+        response = hello.views.test(None, "facebook", "MSFT")
+        self.assertTrue(response.status_code == 404)
